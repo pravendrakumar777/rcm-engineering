f95da9dc175cfe8a3c7050fc69a9f7539139792d
Pravendra Kumar <pravendra.kumar@outlook.com>
2025-09-13 17:40:20 +0530

    jenkins and profiling changes

--- src/main/java/com/rcm/engineering/RcmEngineeringApplication.java
+++ src/main/java/com/rcm/engineering/RcmEngineeringApplication.java
@@ -1,10 +1,24 @@
 package com.rcm.engineering;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+
 @SpringBootApplication
 public class RcmEngineeringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RcmEngineeringApplication.class, args);
 	}
+
+    @Autowired
+    private Environment env;
+
+    @PostConstruct
+    public void checkProfile() {
+        System.out.println("Active profiles: " + Arrays.toString(env.getActiveProfiles()));
+    }
 }
